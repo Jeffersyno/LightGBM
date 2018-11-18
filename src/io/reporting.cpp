6867a1a8a26d0e8baa98a952a937c17dbe28341a
Laurens Devos <laurens.devos@outlook.com>
2018-11-18 15:11:21 +0100

    Pseudo random subset selection

--- src/io/reporting.cpp
+++ src/io/reporting.cpp
@@ -1,12 +1,17 @@
 #include <ios>
 #include <iomanip>
 
-#include <stdlib.h>
-
 #include <LightGBM/reporting.h>
 #include <LightGBM/utils/log.h>
 
 namespace LightGBM {
+
+/* deterministic random numbers */
+float myrand(size_t i) {
+    int p = 4637;
+    int a = (1234 + (static_cast<int>(i % p) * 4309) % p) % p;
+    return (a * 1.0) / p;
+}
 
 FloatWriter::FloatWriter(const std::string &filename, double sample_prob)
     : sample_prob_(sample_prob)
@@ -22,7 +27,7 @@ FloatWriter::WriteFloats(size_t num_floats, F const *floats)
 {
     size_t count = 0;
     for (size_t i = 0; i < num_floats; ++i) {
-        auto r = (rand() * 1.0) / RAND_MAX;
+        auto r = (myrand(i) * 1.0) / RAND_MAX;
         if (r <= sample_prob_) {
             file_ << std::scientific << std::setprecision(4) << floats[i] << ' ';
             ++count;
